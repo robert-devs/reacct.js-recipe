a43010d164a9c6aa5d2a364e5ad4e9f7aef1f09f
robert <mainrobert04@gmail.com>
2022-06-18 15:03:46 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Pages from "./Pages/Pages";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello there</h1>
+      <Pages />
     </div>
   );
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import Veggie from "../componets/Veggie";
+import Popular from "../componets/Popular";
+
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Veggie />
+      <Popular />
+    </div>
+  );
+}
+
+export default Home;
